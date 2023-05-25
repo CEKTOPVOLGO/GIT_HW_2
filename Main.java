144cc769acb4025b0806f306c9f178f153a7975a
MainUser <berestnev86@gmail.com>
2023-05-26 02:31:51 +0300

    Revert "Добавил приветствие" Приветствие удалено
    This reverts commit 273d40a761528b37ed6744e37b72ed17474f951c.

--- Main.java
+++ Main.java
@@ -23,7 +23,6 @@ public class Main {
         laptops.add(laptop4);
         laptops.add(laptop5);
         laptops.add(laptop6);
-        System.out.println("Привет");
         System.out.println("Здравствуйте, в наличии имеются следующие ноутбуки:");
         for (Laptop laptop : laptops) {
             System.out.println(laptop);
